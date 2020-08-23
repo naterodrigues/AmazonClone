05ddf1c598e60b064471e8d9551dbcd48cfbf71e
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-23 18:58:37 +1000

    Update for header navigation

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -4,6 +4,7 @@ import React from 'react';
 import "./Header.css";
 import {Link} from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 
 function Header() {
@@ -26,38 +27,46 @@ function Header() {
             <div className="header__nav">
                 {/*First link */}
 
-                <Link to ="/login" className="header__link"> 
-                <div className="header__option">
-                <span>Hello Nate</span>
-                <span>Sign In</span>
-                </div>
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Nate</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
                 </Link>
 
 
                 {/*Second link */}
 
 
-                <Link to ="/" className="header__link"> 
-                <div className="header__option">
-                <span>Returns</span>
-                <span>& Orders</span>
-                </div>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
                 </Link>
 
                 {/*Third link */}
 
 
-                <Link to ="/" className="header__link"> 
-                <div className="header__option">
-                <span>Your</span>
-                <span>Prime</span>
-                </div>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
                 </Link>
 
-                {/*Fourth link */} </div>
+                {/*Fourth link */}
 
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        {/* Shopping Basket Icon*/}
+                        <ShoppingBasketIcon className="header__basketIcon"></ShoppingBasketIcon>
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
 
-            {/*shopping cart icon, with items in cart */} </nav>
+            </div>
+        </nav>
     )
 }
 
