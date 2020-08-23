be21039f79622d1e0c0d432b301ef402f8853abc
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-23 17:21:37 +1000

    update

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom" // Adding support for multiple page URLs
+import Header from './Header.js';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+
+                <Switch>
+                  <Route path ="/checkout">
+                    <h1>Checkout</h1>
+                  </Route>
+                  <Route path="/login">
+                    <h1>Login Page</h1>
+                  </Route>
+                  <Route path ="/"> 
+                  <Header /> 
+                    <h1>Home Page</h1>
+                  </Route>
+
+                </Switch>
+
+                {/*we need react-Router for the below */}
+                {/* localhost.com/*/}
+                {/* localhost.com/checkout/*/}
+                {/* localhost.com/login*/} </div>
+        </Router>
+    );
 }
 
 export default App;
