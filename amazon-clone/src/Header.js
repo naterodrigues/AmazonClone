4b529a68f8c8b6e3bee90c6795386ddb83155421
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-23 18:05:50 +1000

    update

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,13 +2,24 @@
 
 import React from 'react';
 import "./Header.css";
+import { Link } from "react-router-dom";
+
 
 function Header() {
     return (
         <nav className="header">
-{/* logo on the left -> img tg */}
+            
+{/* logo on the left -> img tag */}
+<Link to ="/">
 <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon Logo" />
+</Link>
+
 { /* search box */}
+<div className ="header__search">
+<input type ="text" className ="header__searchInput"/>
+
+</div>
+
 {/* three links - hello/sign-in, returns & orders, your prime */}
 {/*shopping cart icon, with items in cart */}
 
