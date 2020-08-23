d737abd213cc6bca2580fdba5651bf26c478b80d
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-23 20:12:17 +1000

    Adding the banner

---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Product.css';
+
+function Product() {
+    return (
+        <div>
+            <h1>I'm a product function</h1>
+        </div>
+    )
+}
+
+export default Product
